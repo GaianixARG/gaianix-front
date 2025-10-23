16eb87fd5cef160caf276518b7ad4722b9d302bb
Francisco <fnasif@fi.uba.ar>
2025-10-23 11:01:27 +0200

    borrado mocks

--- src/constants/mocks/userData.ts
+++
@@ -1,9 +0,0 @@
-import type { IUser } from "../interfaces";
-
-export const USUARIO_SESION: IUser = {
-  id: 1,
-  name: "Francisco",
-  email: "fran@gaianix.com",
-  username: "fran",
-  role: "Administrador",
-};
