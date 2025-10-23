90f7c94cf4ad2d3bd8a17b04cff2bfc83ae28fa9
Francisco <fnasif@fi.uba.ar>
2025-10-23 11:24:03 +0200

    fix check - provider loading sacado de main

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./routes/AppRouter.tsx";
-import { LoadingProvider } from "./providers/LoadingProvider.tsx";
 import AlertContainter from "./components/ui/Alert/AlertContainer.tsx";
 import GlobalLoader from "./components/ui/GlobalLoader.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <LoadingProvider>
-        <GlobalLoader />
-        <AlertContainter />
-        <AppRouter />
-    </LoadingProvider>
+    <GlobalLoader />
+    <AlertContainter />
+    <AppRouter />
   </BrowserRouter>
 );
